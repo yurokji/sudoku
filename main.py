ceeab5eeb379698eba2d016479e987591e5e3bbb
Yurok Ji <49470364+yurokji@users.noreply.github.com>
2022-03-23 15:05:35 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,358 +1,155 @@
-from symbol import except_clause
-from numpy import False_
 import pygame
-from pygame.locals import KEYDOWN, K_0, K_1, K_2, K_3, K_4, K_5, K_6, K_7, K_8, K_9, MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEWHEEL, QUIT
-from random import randint, sample
-from copy import deepcopy
+from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP, QUIT, KEYDOWN, K_r, K_s
+from sudoku import *
 
-idx_numbers = list(range(0, 9))
-sample_numbers = list(range(1, 10))
+
+su = Sudoku()
+su.generate()
+
+# 파이게임을 이용하여 게임 시작
 cell_width = 100
-grid_size = 9
-w, h = cell_width * grid_size, cell_width * grid_size
-grid_data = []
-grid_sample_numbers = []
-
-
-def init_grid():
-    global grid_size, grid_data
-    grid_data = []
-    for i in range(grid_size):
-        row_data = []
-        for j in range(grid_size):
-            row_data.append(0)
-        grid_data.append(row_data)
-
-
-def getRow(grid, i):
-    return grid[i]
-
-
-def getCol(grid, i):
-    col = []
-    for row in grid:
-        col.append(row[i])
-    return col
-
-def setRow(grid,new_row, pos):
-    grid[pos] = deepcopy(new_row)
-    
-def setCol(grid, new_col, pos):
-    for i in range(9):
-        grid[i][pos] = new_col[i]
-            
-    
-
-
-def getSubGrid(grid, x, y):
-    begin_i = x * 3
-    end_i = begin_i + 3
-    begin_j = y * 3
-    end_j = begin_j + 3
-    sub_grid = []
-    for i in range(begin_i, end_i):
-        sub_row = []
-        for j in range(begin_j, end_j):
-            sub_row.append(grid[i][j])
-        sub_grid.append(sub_row)
-    return sub_grid
-
-
-def setSubGrid(grid, new_sublist, x, y):
-    begin_i = x * 3
-    end_i = begin_i + 3
-    begin_j = y * 3
-    end_j = begin_j + 3
-    for i in range(begin_i, end_i):
-        sub_row = []
-        for j in range(begin_j, end_j):
-            grid[i][j] = deepcopy(new_sublist[i * 3 + j])
-
-
-def flatten2D(grid):
-    res = []
-    for row in grid:
-        res.extend(row)
-    return res
-        
-                
-
-init_grid()
-
-backtracks = 0
-
-import random
-def solveSudoku(grid, i=0, j=0):
-    global backtracks
-    i, j = findNextCellToFill(grid)
-    if i == -1:
-        return True
-    random_list = random.sample(range(1,10),9)
-    for e in random_list:
-        if isValid(grid, i, j, e):
-            grid[i][j] = e
-            if solveSudoku(grid, i, j):
-                return True
-            backtracks += 1
-        grid[i][j] = 0
-    return False
-
-
-def findNextCellToFill(grid):
-    for x in range(0, 9):
-        for y in range(0, 9):
-            if grid[x][y] == 0:
-                return x, y
-    return -1, -1
-
-
-def isValid(grid, i, j, e):
-    rowOk = all([e != grid[i][x] for x in range(9)])
-    if rowOk:
-        columnOk = all([e != grid[x][j] for x in range(9)])
-        if columnOk:
-            secTopX, secTopY = 3 * (i//3), 3 * (j//3)
-            for x in range(secTopX, secTopX + 3):
-                for y in range(secTopY, secTopY + 3):
-                    if grid[x][y] == e:
-                        return False
-            return True
-    return False
-
-
-def printSudoku(grid):
-    numrow = 0
-    for row in grid:
-        if numrow % 3 == 0 and numrow != 0:
-            print(' ')
-        print(row[0:3], ' ', row[3:6], ' ', row[6:9])
-        numrow += 1
-    print()
-
-
-def deleteSoduke(grid, user_places, fixed_data):
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            rnum = randint(0, 20)
-            if rnum >19:
-                grid[i][j] = 0
-                user_places.append((i,j))
-            else:
-                fixed_data.append((i,j))
-
-def isSolved(grid):
-    # row
-    for i in range(9):
-        row = getRow(grid,i)
-        duplicates = [number for number in row if row.count(number) > 1]
-        unique_duplicates = list(set(duplicates))
-        if len(unique_duplicates) > 0:
-            return False
-    print("rows passed!")
-    # col
-    for j in range(9):
-        cols = getCol(grid, j)
-        print(j, cols)
-        duplicates = [number for number in cols if cols.count(number) > 1]
-        unique_duplicates = list(set(duplicates))
-        if len(unique_duplicates) > 0:
-            return False  
-    print("cols passed!")
-    #sub_grid
-    for y in range(3):
-        for x in range(3):
-            sub_grid = getSubGrid(grid, x, y)
-            flat_grid = flatten2D(sub_grid)
-            duplicates = [number for number in flat_grid if flat_grid.count(number) > 1]
-            unique_duplicates = list(set(duplicates))
-            if len(unique_duplicates) > 0:
-                return False 
-    print("subgrids passed!") 
-    return True
-       
-        
-def isSameGrid(grid1, grid2):
-    for i in range(9):
-        for j in range(9):
-            if grid1[i][j] != grid2[i][j]:
-                return False
-    return True
-
-def isAllFilled(grid):
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j] == 0:
-                return False
-    print("all filled!")
-    return True
-            
-def swapsubBlock(grid, h1, h2, horiontal=True):
-    temp = []
-    if horiontal:
-        for i in range(3):
-            temp = deepcopy(grid[h1*3+i])
-            grid[h1*3+i] = deepcopy(grid[h2*3+i])
-            grid[h2*3+i] = deepcopy(temp)
-    else:
-        temp = [0] * 9
-        for j in range(3):
-            for i in range(9):
-                temp[j] = grid[i][h1*3+j]
-                grid[i][h1*3+j] = grid[i][h2*3+j]
-                grid[i][h2*3+j] = temp[j]
-
-def swapRow(grid, r1, r2):
-    row1 = getRow(grid, r1)
-    row2 = getRow(grid, r2)
-    grid[r1] = deepcopy(row2)
-    grid[r2] = deepcopy(row1)
-    
-
-def swapCol(grid, r1, r2):
-    col1 = getCol(grid, r1)
-    col2 = getCol(grid, r2)
-    setCol(grid, col2, r1)
-    setCol(grid, col1, r2)
-
-    
+w, h = cell_width * su.size, cell_width * su.size
+print(w, h)
 pygame.init()
 clock = pygame.time.Clock()
-SURFACE = pygame.display.set_mode((w, h))
+SURFACE = pygame.display.set_mode((w,h))
+
+
+# 보드 이미지
 board_img = pygame.image.load("./images/board.png")
-board_img = pygame.transform.scale(board_img, (w, h))
+board_img = pygame.transform.scale(board_img, (w,h))
 
+# 백그라운드 이미지
 back_img = pygame.image.load("./images/back.png")
-back_img = pygame.transform.scale(back_img, (cell_width, cell_width))
+back_img = pygame.transform.scale(back_img, (w,h))
 
+# 숫자 이미지(유저 클릭가능)
 num_imgs = []
-for i in range(0, grid_size + 1):
+for i in range(0, su.size + 1):
     img = pygame.image.load(f"./images/{str(i)}.png")
     num_imgs.append(pygame.transform.scale(img, (cell_width, cell_width)))
-
+    
+# 고정된 숫자 이미지(유저 클릭 불가능)
 num_fixed_imgs = []
-for i in range(0, grid_size + 1):
-    print(f"./images/{str(i)}f.png")
-    if i==0:
+for i in range(0, su.size + 1):
+    if i==NOT_FILLED:
         img = pygame.image.load(f"./images/{str(i)}.png")
-    else:
+    else:        
         img = pygame.image.load(f"./images/{str(i)}f.png")
     num_fixed_imgs.append(pygame.transform.scale(img, (cell_width, cell_width)))
-    
 
+# 게임 클리어 출력 메시지를 위한 텍스트 렌더링 준비
+font = pygame.font.SysFont('nanumgothicbold',40)  
+text = font.render("정답을 맞추셨습니다",True,(255,0,0))  
+
+        
 running = True
+isClear = False
 pressed = False
 clicked = False
-number = 0
-pos_x, pos_y = 0, 0
-
-init_grid()
-print("init")
-printSudoku(grid_data)
-rlist = sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
-# print(rlist)
-k = 0
-num_block_row = randint(0,2)
-num_block_col = randint(0,2)
-setSubGrid(grid_data, rlist, num_block_row, num_block_col)
-
-print(grid_data)
-solveSudoku(grid_data)
-print("solved")
-printSudoku(grid_data)
-
-num_shuffles = randint(20, 100)
-print("swapping sub bolcks")
-for _ in range(num_shuffles):
-    V1, V2 = sample([0, 1, 2], 2)
-    swapsubBlock(grid_data, V1, V2, horiontal=randint(0, 1))
-    printSudoku(grid_data)
-
-print("swapping rows in subblocks")
-for i in range(num_shuffles):
-    block_row_num = randint(0,2)
-    V1, V2 = sample([0, 1, 2], 2)
-    V1 = V1 + block_row_num * 3
-    V2 = V2 + block_row_num * 3
-    swapRow(grid_data, V1, V2)
-    printSudoku(grid_data)
-
-print("swapping cols in subblocks")
-for i in range(num_shuffles):
-    block_row_num = randint(0,2)
-    V1, V2 = sample([0, 1, 2], 2)
-    V1 = V1 + block_row_num * 3
-    V2 = V2 + block_row_num * 3
-    swapCol(grid_data, V1, V2)
-    printSudoku(grid_data)
-    input()   
-
-printSudoku(grid_data)
-user_places = []
-fixed_places =[]
-deleteSoduke(grid_data, user_places, fixed_places)
-solved_data = deepcopy(grid_data)
-solveSudoku(solved_data)
-
-
+# 마우스를 클릭하면 0에서 9~0~9까지 차례대로 변하도록 한다
+user_number = 0
+# 파이게임 루프
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                init_grid()
-            if event.key == pygame.K_s:
-                solveSudoku(grid_data)
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == KEYDOWN:
+            # r키를 누르면 스도쿠 초기화
+            if event.key == K_r:
+                su.generate()
+            # s키를 누르면 스도쿠 문제 컴퓨터가 풀어줌
+            elif event.key == K_s:
+                su.solve()
+        # 마우스 버튼을 눌렀을 때
+        elif event.type == MOUSEBUTTONDOWN:
             pressed = True
             clicked = False
-        elif event.type == pygame.MOUSEBUTTONUP:
+        # 마우스 버튼을 떼었을 때
+        elif event.type == MOUSEBUTTONUP:
+            # 만약 마우스 버튼을 눌렀다 뗀 것이라면
             if pressed:
+                # 마우스의 위치를 가져온다
                 pos_x, pos_y = pygame.mouse.get_pos()
                 pressed = False
                 clicked = False
-                if pos_x > 10 and pos_x < w - 10 and pos_y > 10 and pos_y < h - 10:
+            # 마우스의 위치가 화면의 10픽셀 안쪽인지 확인한다
+            if pos_x > 10 and pos_x < w - 10 and \
+                pos_y > 10 and pos_y < h - 10:
+                    # 만약 화면 안쪽이라면
+                    # 누른 마우스 좌표를 i,j셀 위치로 환산한다
                     j_q = int(pos_x / cell_width)
                     i_q = int(pos_y / cell_width)
-                    print(solved_data[i_q][j_q])
-                    (i_q, j_q) in user_places
-                    if (i_q, j_q) in user_places:
-                        grid_data[i_q][j_q] = number
-                        number += 1
-                        if number > 9:
-                            number = 1
+                    # 그 위치가 유저가 변환 가능한 셀인지 확인한다
+                    if (i_q, j_q) in su.user_places:
+                        # 가능하다면 현재 number를 유저에게 제시한다
+                        su.grid[i_q][j_q] = user_number
+                        # 같은 셀을 또 다시 클릭할 경우
+                        # 숫자가 1이 올라가도록 한다
+                        user_number += 1
+                        # 숫자가 10이 되었을 경우 다시 1로 바꾸어준다
+                        if user_number > 9:
+                            user_number = 1
+                        # 이미 클릭이 완료된 곳으로 지정한다
                         clicked = True
-                
-
+        
+        
+        
+    #클릭되었다면 스도쿠를 조사하여 게임이 클리어되었는지 살핀다
     if clicked:
-        if isAllFilled(grid_data):
-            if isSolved(grid_data):
-                print("solved !")
-                running = False
-    SURFACE.fill((255, 255, 255))
+        if not isClear:
+            # 우선 스도쿠가 모두 채워졌는지 조사한다
+            if su.isAllFilled():
+                # 그리고 나서 게임이 클리어되었는지 살핀다
+                if su.isSolved():
+                    isClear = True
+
+            
+                
     
-    for i in range(grid_size):
-        for j in range(grid_size):
-            if (i, j) in fixed_places:
-                SURFACE.blit(num_fixed_imgs[grid_data[i][j]],(j * cell_width, i * cell_width))
+    # 그림을 그려주자...드디어ㅜㅠㅠㅜㅜ
+    SURFACE.fill((255,255,255))
+    
+    for i in range(su.size):
+        for j in range(su.size):
+            # 고정된 위치는 고정된 위치의 스프라이트 이미지로 나타내주자
+            if (i,j) in su.fixed_places:
+                SURFACE.blit(num_fixed_imgs[su.grid[i][j]], (j * cell_width, i * cell_width))
+            # 유저가 클릭하여 변경할 수 있는 위치의 스프라이트 이미지를 그린다
             else:
-                SURFACE.blit(num_imgs[grid_data[i][j]],(j * cell_width, i * cell_width))
-    for i in range(9):
-        for j in range(9):
-            rect = (i * cell_width, j*cell_width, cell_width, cell_width)
+                SURFACE.blit(num_imgs[su.grid[i][j]], (j * cell_width, i * cell_width))
+    
+    # 모든 셀의 가장자리를 그려준다
+    for i in range(su.size):
+        for j in range(su.size):
+            rect = (i * cell_width, j * cell_width, cell_width, cell_width)
             pygame.draw.rect(SURFACE, (0,0,0), rect, 2)
-
-
+    
+    # 모든 블록의 가장자리를 두껍게 그려준다
     for i in range(3):
         for j in range(3):
-            rect = (i * cell_width*3, j*cell_width*3, cell_width*3, cell_width*3)
+            rect = (i * cell_width * 3, j * cell_width * 3, cell_width * 3, cell_width * 3)
             pygame.draw.rect(SURFACE, (0,0,0), rect, 5)
-            
     
+    # 게임이 클리어되었다면 메시지를 표시한다
+    if isClear:
+        SURFACE.blit(text,(400,w//2 - 100 ))
+        pygame.display.update()
+        # 3초를 기다린다
+        pygame.time.wait(2000)
+        isClear = False
+        # 게임의 레벨을 올려준다
+        su.difficulty += 4
+        # 게임을 재시작한다
+        su.generate()
+            
     
     pygame.display.update()
     clock.tick(60)
-
-
-pygame.quit()
+            
+            
+        
+                                
+                        
+                
+    
---
+++ sudoku.py
@@ -0,0 +1,391 @@
+from random import randint, sample
+# 리스트를 안전하게 값을 복사하기 위해 사용한다
+from copy import deepcopy
+
+# 아직 결정되지 않은 셀의 값
+NOT_FILLED = 0
+HORIZONTAL_DIRECTION = 0
+VERTICAL_DIRECTION = 1
+BLOCK = 0
+ROW = 1
+COL = 2
+
+class Sudoku:
+    def __init__(self):
+        self.size = 9
+        # 빈칸으로 남겨둘 개수만큼 게임의 난이도
+        self.difficulty = 10
+        self.grid = []
+        # 스도쿠 풀이시 막혔을 때 되돌아갈길의 갯수
+        self.num_backtracks = 0
+        self.num_shuffle = randint(20,100)
+        self.user_places = []
+        self.fixed_places = [] 
+        self.init_grid()
+        
+    #  스도쿠 그리드 초기화
+    def init_grid(self):
+        self.grid = []
+        self.user_places = []
+        self.fixed_places = [] 
+        for i in range(self.size):
+            row_data = []
+            for j in range(self.size):
+                row_data.append(NOT_FILLED)
+            self.grid.append(row_data)
+            
+    # 셀의 값이 채워져있지 않은지 확인
+    def isCellNotFilled(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.grid[i][j] == NOT_FILLED:
+                    return i,j
+        return -1, -1
+    
+    # 숫자 e가 현재 셀 i, j에 위치할 수 있는 지 체크한다
+    def isValid(self, i, j, e):
+        # 숫자 e가 같은 행의 모든 열에서 유일한지 체크한다
+        rowOk = all([e != self.grid[i][x] for x in range(9)])
+        if rowOk:
+            # 숫자 e가 같은 열의 모든 행에서 유일한지 체크한다
+            colOk = all([e != self.grid[y][j] for y in range(9)])
+            if colOk:
+                # 숫자 erk 각각의 작은 정사각형 블록내에서도 
+                # 유일한지 체크한다
+                blockTopX, blockTopY = 3 * (i // 3), 3 * (j //3)
+                for x in range(blockTopX, blockTopX + 3):
+                    for y in range(blockTopY, blockTopY + 3):
+                        if self.grid[x][y] == e:
+                            return False
+                return True
+        return False
+                
+            
+    
+    # 주어진 스도쿠 문제에 대한 해답
+    def solve(self, i=0, j=0):
+        i, j = self.isCellNotFilled()
+        # 모든 셀이 다 채워져 값이 있는 경우
+        # 다 풀었다고 참값을 반환
+        if i == -1:
+            return True
+        # 아직 다 못푼 경우
+        # 1-9까지의 숫자를 랜덤하게 배열하여 샘플한다
+        random_list = sample(range(1,10), 9)
+        for e in random_list:
+            # 고른 1-9의 숫자중 하나가 현재 셀에 올 수 있는지
+            # 체크한다
+            if self.isValid(i, j, e):
+                self.grid[i][j] = e
+                # 재귀적으로 현재 시점부터 스도쿠를 푼다
+                if self.solve(i,j):
+                    return True
+                # 만약 해를 못찾았다면
+                # 그 지점에서 백트래킹하기 위해 카운트를 올린다
+                self.num_backtracks += 1
+            # 그리고 현재 위치를 롤백하여 채워지지 않음으로 표시한다
+            self.grid[i][j] = NOT_FILLED
+        return False
+    
+    
+    # 정사각 (x,y)번째 서브 블록을 하나 가져온다
+    def getSubBlock(self, x, y):
+        # 각각의 블록에 따라 셀의 위치 i,j값의 범위를 계산한다
+        begin_i = x * 3
+        end_i = begin_i + 3
+        begin_j = y * 3
+        end_j = begin_j + 3  
+        sub_block = []
+        for i in range(begin_i, end_i):
+            sub_row = []
+            for j in range(begin_j, end_j):
+                sub_row.append(self.grid[i][j])
+            sub_block.append(sub_row)
+        return sub_block
+    
+    
+    # 주어진 정사각 (x,y)번째 서브 블록의 값을 채워준다
+    def setSubBlock(self, new_list, x, y):
+        # 각각의 블록에 따라 셀의 위치 i,j값의 범위를 계산한다
+        begin_i = x * 3
+        end_i = begin_i + 3
+        begin_j = y * 3
+        end_j = begin_j + 3 
+        idx = 0
+        for i in range(begin_i, end_i):
+            for j in range(begin_j, end_j):
+                # print(9 * 3 + j, new_list[idx])
+                self.grid[i][j] = deepcopy(new_list[idx])
+                idx += 1  
+        
+    
+    # 풀이 전에 스도쿠의 일부 셀을 채워준다
+    # 스도쿠의 9개의 작은 정사각 블록 중
+    # 한 군데만 랜덤하게 채워주고 시작한다
+    # 이 때 정사각 블록의 위치도 랜덤하게 정해준다
+    def fillOneBlock(self):
+        rlist = sample(range(1,10), 9)
+        num_block_row = randint(0,2)
+        num_block_col = randint(0,2)
+        # print("rlist:", rlist)
+        self.setSubBlock(rlist, num_block_row, num_block_col)
+    
+    #스도쿠의 현재 상태를 프린트한다
+    def print(self):
+        num_row = 0
+        for row in self.grid:
+            if num_row % 3 == 0 and num_row != 0:
+                print(" ")
+            print(row[0:3], " ", row[3:6], " ", row[6:9])
+            num_row += 1
+        print()
+        print()
+         
+    
+    #정사각 서브 블록 3개 구역(행방향, 또는 열방향) 두개의 위치를 서로 바꿈
+    def swapSubBlock(self, block1, block2, swapdir=HORIZONTAL_DIRECTION):
+        temp = [] 
+        # 수평방향으로 블록3개의 구역을 서로 바꾼다
+        if swapdir==HORIZONTAL_DIRECTION:
+            for i in range(3):
+                row_num1 = block1 * 3 + i
+                row_num2 = block2 * 3 + i
+                self.swapRow(row_num1, row_num2)
+                
+                
+        # 수직방향으로 블록3개의 구역을 서로 바꾼다
+        else:
+            for j in range(3):
+                col_num1 = block1 * 3 + j
+                col_num2 = block2 * 3 + j
+                self.swapCol(col_num1, col_num2)
+                
+                 
+
+    
+    
+    # 행을 가져옴
+    def getRow(self, i):
+        return self.grid[i]
+    
+    # 열을 가져옴
+    def getCol(self, j):
+        col = []
+        for row in self.grid:
+            col.append(row[j])
+        return col
+        
+    # 행에 값을 채움
+    def setRow(self, new_row, pos):
+        self.grid[pos] = deepcopy(new_row)
+        
+    # 열에 값을 채움
+    def setCol(self, new_col, pos):
+        for i in range(self.size):
+            self.grid[i][pos] = new_col[i]
+            
+        
+    
+    # 두 행의 위치를 바꿈
+    def swapRow(self, v1, v2):
+        row1 = self.getRow(v1)
+        row2 = self.getRow(v2)
+        # print(row1, row2)
+        self.grid[v1] = deepcopy(row2)
+        self.grid[v2] = deepcopy(row1)
+        # row1 = self.getRow(v1)
+        # row2 = self.getRow(v2)
+        # print(row1, row2)
+        
+    # 두 열의 위치를 바꿈
+    def swapCol(self, h1, h2):
+        col1 = self.getCol(h1)
+        col2 = self.getCol(h2)
+        # print(col1, col2)
+        self.setCol(col2, h1)
+        self.setCol(col1, h2)
+        # col1 = self.getCol(h1)
+        # col2 = self.getCol(h2)
+        # print(col1, col2)
+        
+        
+         
+    
+    # 풀이가 가능한 스도쿠 데이터가 준비되었음
+    # 이제 이 스도쿠를 기초로 
+    # 몇 가지 셔플 작업을 함
+    # 첫째, 풀이가 가능한 스도쿠 데이터의 정사각 블록 행, 
+    # 정사각 블록 열을 셔플해줌
+    # 블록을 셔플하는 것은 풀이의 무결성을 해치지 않음
+    def shuffle(self, num, type, direction=HORIZONTAL_DIRECTION):
+        for i in range(num):
+             # 셔플의 옵션이 블록이라면
+            if type == BLOCK:
+                # (행/열 방향이 선택된 후) 두 블록을 결정 
+                V1, V2 = sample([0,1,2], 2)
+                # 두 블록의 위치를 바꿈 (랜덤한 방향으로 결정)
+                # print(i, "번 블록 셔플 후")
+                self.swapSubBlock(V1, V2, swapdir=direction)
+                # self.print()
+            # 셔플의 옵션이 행이라면
+            elif type == ROW:
+                block_row_num = randint(0,2)
+                V1, V2 = sample([0,1,2], 2)
+                # 블록의 위치에 따라 행의 위치를 계산
+                V1 = V1 + block_row_num * 3
+                V2 = V2 + block_row_num * 3
+                self.swapRow(V1, V2)
+             # 셔플의 옵션이 열이라면
+            elif type == COL:
+                block_col_num = randint(0,2)
+                V1, V2 = sample([0,1,2], 2)
+                # 블록의 위치에 따라 행의 위치를 계산
+                V1 = V1 + block_col_num * 3
+                V2 = V2 + block_col_num * 3
+                self.swapCol(V1, V2)               
+
+        
+    # 셔플이 완료된 풀이 데이터를 이용해서
+    # 일부의 셀을 삭제하여 문제로 바꾼다 
+    # del_size; 얼마나 지울 것인지에 따라 난이도가 올라간다 
+    def delete(self, del_size=10): 
+        if del_size >= self.size * self.size:
+            del_size = self.size * self.size - 1
+        # 유저가 클릭할 i, j 위치: user_places
+        # 숫자 이미지가 고정되어 들어갈 i,j 위치: fixed_places 
+        self.user_places = []
+        self.fixed_places = []
+        for i in range(self.size):
+            for j in range(self.size):
+                self.fixed_places.append((i,j))
+                 
+        del_count = 0
+        while True:
+            i, = sample(range(self.size), 1)
+            j, = sample(range(self.size), 1)
+            if self.grid[i][j] != NOT_FILLED:
+                self.grid[i][j] = NOT_FILLED
+                self.user_places.append((i,j))
+                self.fixed_places.remove((i,j))
+                del_count += 1
+                if del_count == del_size:
+                    break
+
+                
+    
+    # 스도쿠가 모두 채워졌는지 검사한다
+    def isAllFilled(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.grid[i][j] == NOT_FILLED:
+                    return False
+        return True
+    
+    
+    # 2차원 리스트를 1차원으로 만든다
+    def flatten2D(self, list2d):
+        res = []
+        for row in list2d:
+            res.extend(row)
+        return res
+        
+    
+    
+    # 스도쿠가 클리어되었는지 검사한다
+    def isSolved(self):
+        # 첫째로, 각 행을 살펴 숫자들이 유일한지 검사하여
+        # 모든 행의 숫자가 각각 유일하다면 행조건 통과
+        for i in range(self.size):
+            row = self.getRow(i)
+            #  각 행마다 똑같은 숫자가 2개 이상 있는지 검사
+            dupli = [number for number in row if row.count(number) > 1]
+            # set 연산자를 이용해서 겹치는 숫자 중 1개만 거름 [1, 2,2, 4,4,4] -> [1,2,4]
+            dupli = list(set(dupli))
+            # 만약 겹치는 숫자 1개라도 발견된다면, 클리어 안된 것
+            if len(dupli) > 0:
+                return False
+        print("행 클리어 조건 통과!")
+        
+        # 둘째로, 각 열을 살펴 숫자들이 유일한지 검사하여
+        # 모든 열의 숫자가 각각 유일하다면 열조건 통과
+        for i in range(self.size):
+            col = self.getCol(i)
+            #  각 행마다 똑같은 숫자가 2개 이상 있는지 검사
+            dupli = [number for number in col if col.count(number) > 1]
+            # set 연산자를 이용해서 겹치는 숫자 중 1개만 거름 [1, 2,2, 4,4,4] -> [1,2,4]
+            dupli = list(set(dupli))
+            # 만약 겹치는 숫자 1개라도 발견된다면, 클리어 안된 것
+            if len(dupli) > 0:
+                return False
+        print("열 클리어 조건 통과!")
+        
+        # 셋째로, 정사각 블록 9개를 검사하여
+        # 각 블록 안의 숫자들이 유일한지 검사하여
+        # 유일하다면 블록 조건 통과
+        for y in range(3):
+            for x in range(3):
+                # 블록을 하나 가져온다
+                sub_block = self.getSubBlock(x,y)
+                # 블록을 2차원에서 1차원 리스트로 변환한다
+                flat_list = self.flatten2D(sub_block)
+                # 리스트 안에 똑같은 숫자가 2개 이상 있는지 검사
+                dupli = [number for number in flat_list if flat_list.count(number) > 1]
+                # set 연산자를 이용해서 겹치는 숫자 중 1개만 거름 [1, 2,2, 4,4,4] -> [1,2,4]
+                dupli = list(set(dupli))
+                # 만약 겹치는 숫자 1개라도 발견된다면, 클리어 안된 것
+                if len(dupli) > 0:
+                    return False
+        print("블록 클리어 조건 통과!")
+        return True
+                
+                
+        
+        
+        
+    
+    # 스도쿠 문제 풀이 준비
+    # 난이도는 1: 빈칸 하나만 있는 것
+    # 난이도 최대: 모두 빈칸 81 
+    def generate(self):
+        # 스도쿠 초기화
+        self.init_grid()
+        print("스도쿠 그리드 지우기")
+        self.print()
+        # 스도쿠 한 블록만 채워서 준비
+        print("스도쿠 블록 하나 랜덤하게 채우기")
+        self.fillOneBlock()
+        self.print()
+        # 스도쿠 풀이
+        print("스도쿠 솔루션 하나 만들기")
+        self.solve()
+        self.print()
+
+        # 각각의 서브 정사각 블록 셔플
+        self.shuffle(type=BLOCK, direction=HORIZONTAL_DIRECTION, num=1)
+        self.print()
+
+        # 각각의 서브 정사각 블록 내의 행 3개의 위치 셔플한다
+        self.shuffle(type=ROW, num=100)
+        print("행 셔플후")
+        self.print()
+
+        self.shuffle(type=COL, num=100)
+        print("열 셔플후")
+        self.print()
+        
+        # 셔플이 완료된 풀이 데이터를 이용해서
+        # 일부의 셀을 삭제하여 문제로 바꾼다
+        self.delete(del_size=self.difficulty)
+        # print("일부 셀을 삭제 한 후")
+        # self.print()
+
+
+                    
+        
+
+
+    
+
+ 
+        
+        
